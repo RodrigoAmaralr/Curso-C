0e2c6441ae3624c433cbb7ee17e247ec2f448684
Rodrigo Micromed <rodrigo.com2@gmail.com>
2021-11-19 13:22:54 -0300

    Finalizado Condicionais e Loops com ifs, fors e while

--- main.c
+++ main.c
@@ -10,7 +10,35 @@ int main(){
 
     int chute;
 
-    printf("Qual e o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d", chute);
+    int ganhou = 0;
+    int tentativas = 1;
+
+    while (1){
+        printf("Tentativa %d \n", tentativas);
+        printf("Qual e o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+
+        if(chute < 0){
+            printf("Voce nao pode chutar numeros negativos!\n");
+            continue;
+        }
+        int acertou = (chute == numerosecreto);
+        int maior = chute > numerosecreto;
+
+        if(acertou){
+            printf("Parabens! Voce acertou\n");
+            printf("Jogue de novo, voce e um bom jogador\n");
+            break;
+        }
+        else if(maior){
+            printf("Seu chute foi maior que o numero secreto\n");
+        }
+        else{
+            printf("Seu chute foi menor que o numeor secreto\n");
+        }
+        tentativas++;
+    }
+    printf("Fim de Jogo!");
+    printf("Voce acertou em %d tentativas!", tentativas);
 }
